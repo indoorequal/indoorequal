47098d8b2d6b6cd936fe0ea1c0d812e69d0da224
François de Metz <francois@2metz.fr>
2020-06-24 14:41:16 +0200

    Merge unwalled areas.

--- layers/area/layer.sql
+++ layers/area/layer.sql
@@ -4,25 +4,53 @@
 DROP FUNCTION IF EXISTS layer_indoor(geometry, integer, numeric);
 CREATE FUNCTION layer_indoor(bbox geometry, zoom_level integer, pixel_width numeric)
 RETURNS TABLE(id integer, geometry geometry, class text, level numeric, access text) AS $$
-   SELECT
-        id,
-        geometry,
-        class,
-        unnest(array_cat(
-                string_to_array(level, ';'),
-                repeat_on_to_array(repeat_on)
-        ))::numeric as level,
-        access
-   FROM (
-      -- etldoc: osm_indoor_polygon -> layer_indoor:z14_
-      SELECT id, geometry, class, level, repeat_on, access
-      FROM osm_indoor_polygon
-      WHERE zoom_level >= 14 AND geometry && bbox
-      UNION ALL
-      -- etldoc: osm_indoor_linestring -> layer_indoor:z14_
-      SELECT id, geometry, class, level, repeat_on, NULL as access
-      FROM osm_indoor_linestring
-      WHERE zoom_level >= 14 AND geometry && bbox
-   ) AS indoor_all;
+  WITH
+  areas AS (
+    SELECT
+          id,
+          geometry,
+          class,
+          unnest(array_cat(
+                  string_to_array(level, ';'),
+                  repeat_on_to_array(repeat_on)
+          ))::numeric as level,
+          access,
+          tags
+     FROM (
+        -- etldoc: osm_indoor_polygon -> layer_indoor:z14_
+        SELECT id, geometry, class, level, repeat_on, access, tags
+        FROM osm_indoor_polygon
+        WHERE zoom_level >= 14 AND geometry && bbox
+        UNION ALL
+        -- etldoc: osm_indoor_linestring -> layer_indoor:z14_
+        SELECT id, geometry, class, level, repeat_on, NULL as access, NULL AS tags
+        FROM osm_indoor_linestring
+        WHERE zoom_level >= 14 AND geometry && bbox
+     ) AS indoor_all
+   ),
+   unwalled_areas AS (
+     SELECT *
+     FROM areas
+     WHERE class IN ('area', 'corridor', 'platform')
+   )
 
+   SELECT *
+   FROM (
+     SELECT -1 AS id, ST_UNION(geometry) AS geometry, 'area' as class, level, access
+     FROM unwalled_areas
+     WHERE NOT tags ?| ARRAY['amenity', 'shop', 'craft', 'leisure', 'office', 'sport', 'tourism', 'exhibit']
+     GROUP BY level, access
+
+     UNION ALL
+
+     SELECT id, geometry, class, level, access
+     FROM unwalled_areas
+     WHERE tags ?| ARRAY['amenity', 'shop', 'craft', 'leisure', 'office', 'sport', 'tourism', 'exhibit']
+
+     UNION ALL
+
+     SELECT id, geometry, class, level, access
+     FROM areas
+     WHERE class NOT IN ('area', 'corridor', 'platform')
+   ) AS indoor_merged;
 $$ LANGUAGE SQL IMMUTABLE;
