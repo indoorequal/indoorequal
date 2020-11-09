844ada8d1fc180fd728cfff502fa6885e82cdb15
Adrien Pavie <panieravide@riseup.net>
2020-11-09 16:40:08 +0100

    Add new layer in tileset for heatmaps (#15)
    * Add new layer in tileset for heatmaps
    
    * Max zoom 17 for heatmap
    
    * Update docs

---
+++ layers/heat/layer.sql
@@ -0,0 +1,14 @@
+
+-- etldoc: layer_heat[shape=record fillcolor=lightpink,
+-- etldoc:     style="rounded,filled", label="layer_heat | <z0_17> z0-17" ] ;
+
+DROP FUNCTION IF EXISTS layer_heat(geometry);
+DROP FUNCTION IF EXISTS layer_heat(geometry, integer);
+CREATE FUNCTION layer_heat(bbox geometry, zoom_level integer)
+RETURNS TABLE(id bigint, geometry geometry) AS $$
+   -- etldoc: osm_area_heatpoint -> layer_heat:z0_17
+   SELECT osm_id AS id, geometry
+    FROM osm_area_heatpoint
+    WHERE zoom_level < 17 AND geometry && bbox;
+
+$$ LANGUAGE SQL IMMUTABLE;
