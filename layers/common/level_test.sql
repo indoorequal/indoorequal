1209103f16b8c15895e8c5028cb3f446c8ff892b
François de Metz <francois@2metz.fr>
2021-11-12 14:24:41 +0100

    Add tests on expand_levels.

---
+++ layers/common/level_test.sql
@@ -0,0 +1,30 @@
+BEGIN;
+SELECT plan(5);
+
+SELECT is(
+    expand_levels('1', ''),
+    ARRAY[ 1 ]::numeric[]
+);
+
+SELECT is(
+    expand_levels('1;2', ''),
+    ARRAY[ 1, 2 ]::numeric[]
+);
+
+SELECT is(
+    expand_levels('1', '2'),
+    ARRAY[ 1, 2 ]::numeric[]
+);
+
+SELECT is(
+    expand_levels('1', '2;3'),
+    ARRAY[ 1, 2, 3 ]::numeric[]
+);
+
+SELECT is(
+    expand_levels('1', '2-4'),
+    ARRAY[ 1, 2, 3, 4 ]::numeric[]
+);
+
+SELECT * FROM finish();
+ROLLBACK;
