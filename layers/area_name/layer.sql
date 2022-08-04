6a29ce311d0c9e21b1a73a6cbe97e04a0d876ca3
François de Metz <francois@2metz.fr>
2022-08-04 18:28:18 +0200

    Add a ref field to the area_name.

--- layers/area_name/layer.sql
+++ layers/area_name/layer.sql
@@ -5,12 +5,13 @@
 DROP FUNCTION IF EXISTS layer_area_name(geometry, integer, numeric);
 DROP FUNCTION IF EXISTS layer_area_name(geometry, integer);
 CREATE FUNCTION layer_area_name(bbox geometry, zoom_level integer)
-RETURNS TABLE(geometry geometry, name text, name_en text, name_de text, level numeric, tags hstore) AS $$
+RETURNS TABLE(geometry geometry, name text, name_en text, name_de text, ref text, level numeric, tags hstore) AS $$
    -- etldoc: osm_area_point -> layer_area_name:z17_
    SELECT geometry,
-          COALESCE(NULLIF(name, ''), ref) AS name,
+          COALESCE(NULLIF(name, ''), '') AS name,
           COALESCE(NULLIF(name_en, ''), name) AS name_en,
           COALESCE(NULLIF(name_de, ''), name) AS name_de,
+          COALESCE(NULLIF(ref, ''), '') AS ref,
           unnest(expand_levels(level, repeat_on)) AS level,
           tags
     FROM osm_area_point
