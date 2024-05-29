e056978fb7d299d277eae8089116ddbc6e9b21b0
François de Metz <francois@2metz.fr>
2024-05-29 11:31:31 +0200

    Update rank.

--- layers/poi/class.sql
+++ layers/poi/class.sql
@@ -2,28 +2,20 @@ CREATE OR REPLACE FUNCTION poi_class_rank(class text)
     RETURNS int AS
 $$
 SELECT CASE class
-           WHEN 'hospital' THEN 20
-           WHEN 'railway' THEN 40
-           WHEN 'bus' THEN 50
-           WHEN 'attraction' THEN 70
-           WHEN 'harbor' THEN 75
-           WHEN 'college' THEN 80
-           WHEN 'school' THEN 85
-           WHEN 'stadium' THEN 90
-           WHEN 'zoo' THEN 95
-           WHEN 'town_hall' THEN 100
-           WHEN 'campsite' THEN 110
-           WHEN 'cemetery' THEN 115
-           WHEN 'park' THEN 120
-           WHEN 'library' THEN 130
-           WHEN 'police' THEN 135
-           WHEN 'post' THEN 140
-           WHEN 'golf' THEN 150
-           WHEN 'shop' THEN 400
-           WHEN 'grocery' THEN 500
-           WHEN 'fast_food' THEN 600
-           WHEN 'clothing_store' THEN 700
-           WHEN 'bar' THEN 800
+           WHEN 'telephone' THEN 10
+           WHEN 'information' THEN 10
+           WHEN 'bank' THEN 50
+           WHEN 'office' THEN 70
+           WHEN 'shop' THEN 100
+           WHEN 'clothing_store' THEN 100
+           WHEN 'grocery' THEN 100
+           WHEN 'fast_food' THEN 100
+           WHEN 'laundry' THEN 200
+           WHEN 'cafe' THEN 300
+           WHEN 'bar' THEN 300
+           WHEN 'beer' THEN 300
+           WHEN 'vending_machine' THEN 700
+           WHEN 'entrance' THEN 800
            ELSE 1000
            END;
 $$ LANGUAGE SQL IMMUTABLE
